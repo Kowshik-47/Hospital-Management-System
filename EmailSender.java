3da839b4aeb6b1c80fa2126f8f658d0dc5f3db56
Kowshik-47 <kavshikbabu7_bcs26@mepcoeng.ac.in>
2025-04-25 09:56:24 +0530

    Update EmailSender.java

--- EmailSender.java
+++ EmailSender.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package hospitalmanagementsystem;
 
-/**
- *
- * @author Gopisaran
- */
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
